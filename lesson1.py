4511f7841ddf357a916f1128c937c76809a7528c
lukabendeliani25 <lukabendeliani9@gmail.com>
2022-09-22 18:07:27 +0400

    second commit

---
+++ lesson1.py
@@ -0,0 +1,35 @@
+name = "luka"
+#name არის ცვლადი
+# = არის ცვლადისთის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "ლუკა" არის ცვლადის მნიშვნელობა
+
+surname = "bendeliani"
+
+print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი(ცვლადი ან სტრინგი)
+
+name = "luka" #ეს არის str(string) ტიპის ცვლადი
+age = 19 # ეს არის int (integer) მთელი რიცხვი
+height = 182.5 # ეს არის float ტიპის ცვლადი (ათწილადი)
+#booLean (bool) ტიპის ცვლადი
+
+knows_programing = True #True ან False
+is_dancer = False
+
+isdancer = False # ჯავასკრიპტულუ camelcase
+
+
+print(name + " " + surname)
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+# pringt(name + age)
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_programming))
+
+print(name + " " +str(age))
+
+print(name + " " + surname + " " + str(age) + " " +str(height))
